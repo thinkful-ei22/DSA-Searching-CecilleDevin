cb0a95d2cb7ecccee97d0d3d5c81b98a7b70bcba
Cecille Salazar <cecillersalazar@gmail.com>
2018-09-06 09:36:35 -0700

    Linear Search Drill complete!

--- dsa-search/src/App.js
+++ dsa-search/src/App.js
@@ -7,7 +7,9 @@ class App extends React.Component {
     super(props)
 
     this.state = {
-      input: 0
+      input: 0,
+      inputArr: [89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5],
+      counter: 0
     }
   }
 
@@ -16,6 +18,22 @@ class App extends React.Component {
       input: e
     })
   }
+
+  indexOf(array, value) {
+    let counter = 0;
+    for (let i=0; i<array.length; i++) {
+        counter++;
+        this.setState({
+          counter: counter
+        });
+        if (array[i] == value) {
+            console.log('It took this many iterations: ', counter);
+            return i;
+        }
+    }
+    console.log(`Didn\'t find counter! Searched ${counter} items`);
+    return -1;
+  };
 
   render() {
     return (
@@ -30,9 +48,8 @@ class App extends React.Component {
               Input:
               <input type="text" onChange={(e) => {
                 const val = e.target.value;
-                console.log(val);
                 this.setState({ input: val });
-                console.log(this.state);
+                this.indexOf(this.state.inputArr, 78);
               }}name="name" />
             </label>
             <input type="submit" value="Submit" />
