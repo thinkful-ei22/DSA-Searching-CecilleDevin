16b61ef78463e5df030eef63992bb1ae35adf497
Cecille Salazar <cecillersalazar@gmail.com>
2018-09-06 11:59:06 -0700

    Implemented quicksort

--- dsa-search/src/App.js
+++ dsa-search/src/App.js
@@ -10,9 +10,41 @@ class App extends React.Component {
       input: 0,
       inputArr: [89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5],
       sortMeArr: [89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5],
-      linearCounter: 0
+      linearCounter: 0,
+      inputArr2: [89,30,25,32,72,70,51,42,25,24,53,55,78,50,13,40,48,32,26,2,14,33,45,72,56,44,21,88,27,68,15,62,93,98,73,28,16,46,87,28,65,38,67,16,85,63,23,69,64,91,9,70,81,27,97,82,6,88,3,7,46,13,11,64,76,31,26,38,28,13,17,69,90,1,6,7,64,43,9,73,80,98,46,27,22,87,49,83,6,39,42,51,54,84,34,53,78,40,14,5]
     };
   }
+
+  swap(array, i, j) {
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+
+  partition(array, start, end) {
+      const pivot = array[end - 1];
+      let j = start;
+      for (let i=start; i<end - 1; i++) {
+          if (array[i] <= pivot) {
+              this.swap(array, i, j);
+              j++;
+          }
+      }
+      this.swap(array, end-1, j);
+      return j;
+  };
+
+  quickSort(array, start=0, end=array.length) {
+    if (start >= end) {
+        return array;
+    }
+    const middle = this.partition(array, start, end);
+    array = this.quickSort(array, start, middle);
+    array = this.quickSort(array, middle + 1, end);
+    console.log('Quicksort product');
+    console.log(array);
+    return array;
+  };
 
   onSubmit(e) {
     this.setState({
@@ -82,6 +114,7 @@ class App extends React.Component {
                 this.setState({ input: val });
                 this.indexOf(this.state.inputArr, val);
                 this.binarySearch(this.state.sortMeArr.sort(), val);
+                this.quickSort(this.state.inputArr2);
               }}name="name" />
             </label>
             <input type="submit" value="Submit" />
